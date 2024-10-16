b52f9405351f54f0ff84191ee57803aae2c69221
Xa-C24 <9335@holbertonstudents.com>
2024-10-16 10:32:23 +0200

    Mise à jour avec suppressions et ajouts de fichiers

--- HBnB_partie_2/app_HBnB/Persistence.3/repository.py
+++
@@ -1,2 +0,0 @@
-"""Référentiel en mémoire pour gérer les objets."""
-"""Fournit des méthodes pour ajouter, récupérer, mettre à jour, et supprimer des objets."""
---
+++ HBnB_partie_2/app_HBnB/test/test_amenity.py
@@ -0,0 +1,32 @@
+import unittest
+from datetime import datetime
+
+
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app_HBnB.models.amenity import Amenity
+
+class TestAmenity(unittest.TestCase):
+    def test_amenity_creation(self):
+        """Test la création d'un objet Amenity."""
+        amenity = Amenity(name="Wi-Fi")
+        # Vérifie que le nom est bien "Wi-Fi"
+        self.assertEqual(amenity.name, "Wi-Fi")
+        # Vérifie que created_at est une instance de datetime
+        self.assertIsInstance(amenity.created_at, datetime)
+        # Vérifie que updated_at est une instance de datetime
+        self.assertIsInstance(amenity.updated_at, datetime)
+
+    def test_amenity_update(self):
+        """Test la mise à jour de l'objet Amenity."""
+        amenity = Amenity(name="Wi-Fi")
+        amenity.update({"name": "Free Wi-Fi"})
+        # Vérifie que le nom a été mis à jour correctement
+        self.assertEqual(amenity.name, "Free Wi-Fi")
+
+
+if __name__ == '__main__':
+    unittest.main()
