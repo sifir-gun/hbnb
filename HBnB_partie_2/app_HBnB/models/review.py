b1e3e822567510b55387fdc9e573d30712ffa75a
Neia2 <7276@holbertonstudents.com>
2024-10-16 13:55:41 +0200

    updating user and reviw models, requirements and utils directory

--- HBnB_partie_2/app_HBnB/models/review.py
+++ HBnB_partie_2/app_HBnB/models/review.py
@@ -1,2 +1,37 @@
+import uuid
+from datetime import datetime
+
+
 """Classe Review : représente un avis laissé par un utilisateur sur un place."""
 """Gère les attributs et méthodes associés aux reviews."""
+
+
+class Review:
+    def __init__(self, user_id, place_id, content, rating):
+        self.id = str(uuid.uuid4())
+        self.user_id = user_id
+        self.place_id = place_id
+        self.content = content
+        self.rating = self.validate_rating(rating)
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def validate_rating(self, rating):
+        """Ensures the rating is within the allowed range (1 to 5)."""
+        if 1 <= rating <= 5:
+            return rating
+        else:
+            raise ValueError("Rating must be between 1 and 5")
+
+    def update(self, content=None, rating=None):
+        """Updates the review's attributes in a simple and explicit way."""
+        if content:
+            self.content = content
+        if rating is not None:
+            self.rating = self.validate_rating(rating)
+
+        self.updated_at = datetime.now()
+
+    def save(self):
+        """Updates the `updated_at` timestamp without changing any other data."""
+        self.updated_at = datetime.now()
--- HBnB_partie_2/app_HBnB/models/user.py
+++ HBnB_partie_2/app_HBnB/models/user.py
@@ -1,2 +1,24 @@
+from utils.auth_utils import hash_password, verify_password
+import uuid
+from datetime import datetime
+
 """représente un utilisateur dans le système."""
 """Cette classe gère les attributs et méthodes associés aux utilisateurs."""
+
+
+class User:
+    def __init__(self, username, email, password):
+        self.id = str(uuid.uuid4())
+        self.username = username
+        self.email = email
+        self.password = hash_password(password)
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def check_password(self, password):
+        """Check the user's password using the helper function"""
+        return verify_password(self.password, password)
+
+    def save(self):
+        """Updates the `updated_at` timestamp"""
+        self.updated_at = datetime.now()
