9a397a129aae440509d416cadb977fd4d2bc1994
sifir-gun <guney.tasdelen74@gmail.com>
2024-10-28 16:25:36 +0100

    Resolve merge conflict in app/models/__init__.py

--- part2/app/models/__init__.py
+++ part2/app/models/__init__.py
@@ -1,14 +1,6 @@
 """initialise la couche de logique métier pour l'application HBnB.
 # Cette couche de logique métier définit les entités principales de
 # l'application"""
-<<<<<<< HEAD
-"""
-from app.persistence.repository import InMemoryRepository
-storage = InMemoryRepository()"""
-
-storage = {}
-=======
 
 from app.persistence.repository import InMemoryRepository
 storage = InMemoryRepository()
->>>>>>> b116b12ffdbc6613470b69a774bb93605b29da77
