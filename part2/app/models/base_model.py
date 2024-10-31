befec23f14b78a8faf43117b3fcd5bff029e25dc
sifir-gun <guney.tasdelen74@gmail.com>
2024-10-31 15:32:37 +0100

    modification documentation_compilation lien

--- part2/app/models/base_model.py
+++ part2/app/models/base_model.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 class BaseModel:
     def __init__(self):
-        self.id = str(uuid.uuid4())  # Génère un UUID unique
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
--- part2/config.py
+++ part2/config.py
@@ -6,8 +6,10 @@ class Config:
     Base configuration class with common settings.
 
     Attributes:
-        SECRET_KEY (str): Secret key for application security, retrieved from environment variables.
-        DEBUG (bool): Flag indicating if debugging is enabled; default is False.
+        SECRET_KEY (str): Secret key for application security, retrieved from
+        environment variables.
+        DEBUG (bool): Flag indicating if debugging is enabled; default is
+        False.
     """
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
@@ -17,7 +19,8 @@ class DevelopmentConfig(Config):
     """
     Development configuration that enables debugging.
 
-    Inherits from the base Config class and overrides settings specific to a development environment.
+    Inherits from the base Config class and overrides settings specific to a
+    development environment.
     """
     DEBUG = True
 
